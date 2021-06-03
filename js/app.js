ac5a016039c92899e97747825587daa5246ffb9e
Fall77 <arielalejandrojoelseco@gmail.com>
2021-06-03 01:13:07 -0300

    added api key to window.process.env in init.js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+const api_key = process.env.API_KEY;
+
 document.querySelector('#getData').addEventListener('click', getData);
+
+function getData(){
+
+    let params = new URLSearchParams({
+        access_key: api_key,
+        query: 'Argentina',
+        units: 'm'
+    })
+
+
+    fetch(`http://api.weatherstack.com/current?${params}`)
+    .then((res) => res.json())
+    .then((data) => {
+            document.querySelector('.countryField').innerHTML = `${JSON.stringify(data.location.country)}`;
+            document.querySelector('.humidityField').innerHTML = `${JSON.stringify(data.current.humidity)}%`;
+            document.querySelector('.feelsLikeField').innerHTML = `${JSON.stringify(data.current.feelslike)} C°`;
+            document.querySelector('.temperatureField').innerHTML = `${JSON.stringify(data.current.temperature)} C°`;
+            document.querySelector('.descriptionField').innerHTML = `${JSON.stringify(data.current.weather_descriptions[0])}`;
+            document.querySelector('.windSpeedField').innerHTML = `${JSON.stringify(data.current.wind_speed)} km/h `;
+            document.querySelector('.windSpeedField').innerHTML += `Direction: ${JSON.stringify(data.current.wind_dir)}`;
+        })
+        .catch((err) => console.log(err))
+}
